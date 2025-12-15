2985db80589f1de3d61a962f8c111d6e5e6a4f62
kamranhabibov-ops <kamran.habibov@gmail.com>
2025-12-15 15:49:19 +0400

    theory_lesson_03a

--- src/Theory_lessons/theory_lesson_03a.java
+++ src/Theory_lessons/theory_lesson_03a.java
@@ -3,16 +3,54 @@ package Theory_lessons;
 import java.util.Scanner;
 
 public class theory_lesson_03a {
-    static void main(String args[]) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Dərəcəni daxil edin");
-        int temperature = scanner.nextInt();
+    public static void main(String args[]) {
+        //     Scanner scanner=new Scanner(System.in);
+        //     System.out.println("Dərəcəni daxil edin");
+        //     int temperature = scanner.nextInt();
 
-        if (temperature > 25) {
-            System.out.println("hava istidir");
-        } else {
-                            System.out.println("hava sərindir");
-            }
+        //     if (temperature > 25) {
+        //         System.out.println("hava istidir");
+        //     } else {
+        //                         System.out.println("hava sərindir");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Şəhəri daxil edin (Bakı, Sumqayıt, Xırdalan): ");
+        String city = scanner.nextLine();
+
+        double deliveryPrice = 0;
+        boolean serviceAvailable = true;
+
+
+        switch (city) {
+            case "Bakı":
+                deliveryPrice = 5;
+                System.out.println("Çatdırılma: 5 AZN");
+                break;
+            case "Sumqayıt":
+                deliveryPrice = 7;
+                System.out.println("Çatdırılma: 7 AZN");
+                break;
+            case "Xırdalan":
+                deliveryPrice = 6;
+                System.out.println("Çatdırılma: 6 AZN");
+                break;
+            default:
+                System.out.println("Xidmət mövcud deyil");
+
         }
+
+        if (serviceAvailable) {
+            System.out.println("Sifariş məbləğini daxil edin:");
+            double orderAmount = scanner.nextDouble();
+            if (orderAmount > 50) {
+                deliveryPrice = 0;
+                 }
+            double totalPrice = orderAmount + deliveryPrice;
+            System.out.println("Yekun qiymət çatdırılma daxil: " + (totalPrice) + " AZN");
+
+        }
+        scanner.close();
+
     }
 
+}
