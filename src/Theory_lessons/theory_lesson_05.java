cf9f110e1d1475ceeb68b3ae91e000566251b149
kamranhabibov-ops <kamran.habibov@gmail.com>
2025-12-18 16:10:37 +0400

    theory_lesson_5

--- src/Theory_lessons/theory_lesson_05.java
+++ src/Theory_lessons/theory_lesson_05.java
@@ -3,12 +3,36 @@ package Theory_lessons;
 public class theory_lesson_05 {
     public static void main(String[] args) {
 
-        // weather temperature 1 week
-        String[] temperature = new String[]{"16.5, 15.4, 12.3, 13.2, 13.8, 14.5, 14.1, 13.5"};
+// Ev Tapşırığı: "Hava Analizatoru"
 
-        for (String Temp : temperature) {
-            System.out.print(Temp);
+        double[] temperatures = {20.5, 22.1, 19.0, 18.5, 19.2, 16.0, 17.8};
+        String[] weekDays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        double sum = 0.0;
+
+        System.out.println("Temperatures by day: ");
+        for (int i = 0; i < temperatures.length; i++) {
+            System.out.println(weekDays[i] + " : " + temperatures[i] + "°C");
+        }
+        for (int i = 0; i < temperatures.length; i++) {
+            sum = sum + temperatures[i];
+        }
+        double average = sum / temperatures.length;
+        System.out.println();
+        System.out.println("Average temperature: " + String.format("%.2f", average) + "°C");
+
+        double max = temperatures[0];
+        int maxIndex = 0;
+
+        for (int i = 1; i < temperatures.length; i++) {
+            if (temperatures[i] > max) {
+                max = temperatures[i];
+                maxIndex = i;
+            }
 
         }
+        System.out.println();
+        System.out.println("Highest Temperature:"  + max + " °C (" + weekDays[maxIndex] + ")");
     }
-}
+        }
+
+
